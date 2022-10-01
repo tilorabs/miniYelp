60eb19ccee2540203456a02544172790387a2229
Tilo Rabs <trabs@web.de>
2022-10-01 10:54:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import db from "./controller/pg_controller.js";
+import cors from 'cors';
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = process.env.PORT || 8080;
 
 app.get('/cities', (req, res) => {
